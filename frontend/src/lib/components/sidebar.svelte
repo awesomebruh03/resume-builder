40cd6a1e0edcb8142ea672ae5823443fff966f6d
Md Sazid Ullah <123637515+awesomebruh03@users.noreply.github.com>
2026-01-08 20:41:02 +0000

    feat: integrate Tailwind CSS into the frontend

--- frontend/src/lib/components/sidebar.svelte
+++ frontend/src/lib/components/sidebar.svelte
@@ -1,2 +1,2 @@
-<h1>hello</h1>
+<h1 class="text-red-500">hello</h1>
 <h2>what do you say? lets fuck?</h2>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
